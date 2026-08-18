cbe49e0889637248001a6ed8ad4bff13f2b90c9f
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-18 13:02:37 +0530

    Added basic pytest playwright script

---
+++ tests/test_login_ui.py
@@ -0,0 +1,20 @@
+from playwright.sync_api import sync_playwright, expect
+
+
+class TestLoginUI:
+    def test_title(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(channel="chrome", headless=False)
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto("https://opensource-demo.orangehrmlive.com/")
+            expect(page).to_have_title("OrangeHRM")
+
+    def test_header(self):
+        with sync_playwright() as p:
+            browser = p.chromium.launch(channel="chrome", headless=False)
+            context = browser.new_context()
+            page = context.new_page()
+            page.goto("https://opensource-demo.orangehrmlive.com/")
+
+            # Assert the Login Header 
