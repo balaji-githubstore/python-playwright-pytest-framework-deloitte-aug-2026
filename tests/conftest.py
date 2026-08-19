3ee8cb84b3edda532b65ee51367a134d87b7963d
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-19 10:51:35 +0530

    Updated conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,8 @@ from playwright.sync_api import sync_playwright, expect
 import pytest
 
 
-@pytest.fixture(scope="function", autouse=True)
-def setup():
+@pytest.fixture(scope="function")
+def page():
     with sync_playwright() as p:
         browser = p.chromium.launch(channel="chrome", headless=False)
         context = browser.new_context()
