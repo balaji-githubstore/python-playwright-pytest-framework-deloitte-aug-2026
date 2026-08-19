8d0768ce52444f310febf796c1eef7da17615dad
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-19 13:17:03 +0530

    Updated valid and invalid login test

--- tests/test_login.py
+++ tests/test_login.py
@@ -9,6 +9,8 @@ class TestLogin:
         expect(page.locator(
             "xpath=//p[contains(normalize-space(),'Invalid')]")).to_have_text("Invalid credentials")
 
-    def test_valid_login(self,page: Page):
-        # valid login - Admin and admin123
-        # assert - Quick Launch  text
+    def test_valid_login(self, page: Page):
+        page.locator("xpath=//input[@name='username']").fill("Admin")
+        page.locator("xpath=//input[@name='password']").fill("admin123")
+        page.locator("xpath=//button[normalize-space()='Login']").click()
+        expect(page.locator("xpath=//p[text()='Quick Launch']")).to_have_text("Quick Launch")
