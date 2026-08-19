7ec4159f47782fc4aba2939cd8b09c7c62e09325
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-19 10:24:15 +0530

    Added fixtures

--- tests/test_login_ui.py
+++ tests/test_login_ui.py
@@ -1,20 +1,21 @@
 from playwright.sync_api import sync_playwright, expect
+import pytest
 
 
 class TestLoginUI:
-    def test_title(self):
+
+    @pytest.fixture(scope="function",autouse=True)
+    def setup(self):
         with sync_playwright() as p:
             browser = p.chromium.launch(channel="chrome", headless=False)
             context = browser.new_context()
-            page = context.new_page()
-            page.goto("https://opensource-demo.orangehrmlive.com/")
-            expect(page).to_have_title("OrangeHRM")
+            self.page = context.new_page()
+            self.page.goto("https://opensource-demo.orangehrmlive.com/")
+            yield
+            browser.close()
+
+    def test_title(self):
+        expect(self.page).to_have_title("OrangeHRM")
 
     def test_header(self):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(channel="chrome", headless=False)
-            context = browser.new_context()
-            page = context.new_page()
-            page.goto("https://opensource-demo.orangehrmlive.com/")
-            expect(page.locator("xpath=//h5[text()='Login']")).to_have_text("Login")
-
+        expect(self.page.locator("xpath=//h5[text()='Login']")).to_have_text("Login")
