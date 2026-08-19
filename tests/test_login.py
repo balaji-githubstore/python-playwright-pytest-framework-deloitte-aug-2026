2870d001e558f0603ffbbc4ca9dd4e346aaf9cc2
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-19 13:27:21 +0530

    Pytest parameterize for data driven

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,16 +1,23 @@
 from playwright.sync_api import expect, Page
+import pytest
 
 
 class TestLogin:
-    def test_invalid_login(self, page: Page):
-        page.locator("xpath=//input[@name='username']").fill("john")
-        page.locator("xpath=//input[@name='password']").fill("john123")
+
+    @pytest.mark.parametrize("username, password, expected_error", [
+        ["saul", "saul123", "Invalid credentials"],
+        ["kim", "kim123", "Invalid credentials"]
+    ])
+    def test_invalid_login(self, page: Page, username, password, expected_error):
+        page.locator("xpath=//input[@name='username']").fill(username)
+        page.locator("xpath=//input[@name='password']").fill(password)
         page.locator("xpath=//button[normalize-space()='Login']").click()
         expect(page.locator(
-            "xpath=//p[contains(normalize-space(),'Invalid')]")).to_have_text("Invalid credentials")
+            "xpath=//p[contains(normalize-space(),'Invalid')]")).to_have_text(expected_error)
 
     def test_valid_login(self, page: Page):
         page.locator("xpath=//input[@name='username']").fill("Admin")
         page.locator("xpath=//input[@name='password']").fill("admin123")
         page.locator("xpath=//button[normalize-space()='Login']").click()
-        expect(page.locator("xpath=//p[text()='Quick Launch']")).to_have_text("Quick Launch")
+        expect(page.locator(
+            "xpath=//p[text()='Quick Launch']")).to_have_text("Quick Launch")
