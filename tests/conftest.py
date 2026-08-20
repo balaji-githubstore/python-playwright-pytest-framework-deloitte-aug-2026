22217c18b69dcc91129a3480c7cf81a45a32ec35
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-20 09:56:49 +0530

    Updated browser instance for session

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,18 @@ from playwright.sync_api import sync_playwright, expect
 import pytest
 
 
-@pytest.fixture(scope="function")
-def page():
+@pytest.fixture(scope="session")
+def browser():
     with sync_playwright() as p:
         browser = p.chromium.launch(channel="chrome", headless=False)
-        context = browser.new_context()
-        page = context.new_page()
-        page.goto("https://opensource-demo.orangehrmlive.com/")
-        yield page
+        yield browser
         browser.close()
 
+
+@pytest.fixture(scope="function")
+def page(browser):
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://opensource-demo.orangehrmlive.com/")
+    yield page
+    page.close()
