cccc36c4c6a8f6a8f26a18d7ee2ed168f83a37c7
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-20 11:21:04 +0530

    Updated data_reader

--- utilities/data_reader.py
+++ utilities/data_reader.py
@@ -1,7 +1,7 @@
 import pandas
 
 
-def get_csv_into_list(csv_file_path):
+def get_csv_into_list(csv_file_path:str):
     df = pandas.read_csv(filepath_or_buffer=csv_file_path, delimiter=";")
     return df.values.tolist()
 
