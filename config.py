4f643a695dc2be9f49cec60f7db46359b06f125e
balaji-githubstore <balajidinakaran1@gmail.com>
2026-08-20 11:14:02 +0530

    Added Config details

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    ROOT_DIR=os.path.dirname(os.path.abspath(__file__))
+    DATA_DIR=os.path.join(ROOT_DIR,"test_data")
+    REPORT_DIR=os.path.join(ROOT_DIR,"reports")
+
+
+
